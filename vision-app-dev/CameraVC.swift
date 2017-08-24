5c493b03164923fbc53007913bdc9ef14e92d324
Daniel Trask <d.trask@lecreuset.co.jp>
2017-08-24 14:30:54 +0900

    first commit

---
+++ vision-app-dev/CameraVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vision-app-dev
+//
+//  Created by trasksystems on 2017/08/24.
+//  Copyright © 2017 SysamicKK. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
